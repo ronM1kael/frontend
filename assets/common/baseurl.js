3f7717b814dce679278fce57a98753ccd5325b24
Ron Michael Dejan <ronmichael.dejan@tup.edu.ph>
2024-04-27 23:41:54 +0800

    mobile build

--- assets/common/baseurl.js
+++ assets/common/baseurl.js
@@ -1,27 +1,27 @@
-// import { Platform } from 'react-native'
-
-
-// let baseURL = '';
-
-// {Platform.OS == 'android'
-// ? baseURL = 'http://192.168.68.116:8000/api/'
-// : baseURL = 'http://192.168.68.116:8000/api/'
-// }
-
-// export const baseURL2 = 'http://192.168.68.116:8000'
-
-// export default baseURL;
-
 import { Platform } from 'react-native'
 
 
 let baseURL = '';
 
 {Platform.OS == 'android'
-? baseURL = 'http://reachtupt.online/api/'
-: baseURL = 'http://reachtupt.online/api/'
+? baseURL = 'http://192.168.68.116:8000/api/'
+: baseURL = 'http://192.168.68.116:8000/api/'
 }
 
-export const baseURL2 = 'http://reachtupt.online'
+export const baseURL2 = 'http://192.168.68.116:8000'
 
 export default baseURL;
+
+// import { Platform } from 'react-native'
+
+
+// let baseURL = '';
+
+// {Platform.OS == 'android'
+// ? baseURL = 'http://reachtupt.online/api/'
+// : baseURL = 'http://reachtupt.online/api/'
+// }
+
+// export const baseURL2 = 'http://reachtupt.online'
+
+// export default baseURL;
--- assets/common/baseurlnew.js
+++ assets/common/baseurlnew.js
@@ -1,23 +1,23 @@
-// import { Platform } from 'react-native'
-
-
-// let baseURL2 = '';
-
-// {Platform.OS == 'android'
-// ? baseURL2 = 'http://192.168.68.116:8000'
-// : baseURL2 = 'http://192.168.68.116:8000'
-// }
-
-// export default baseURL2;
-
 import { Platform } from 'react-native'
 
 
 let baseURL2 = '';
 
 {Platform.OS == 'android'
-? baseURL2 = 'http://reachtupt.online'
-: baseURL2 = 'http://reachtupt.online'
+? baseURL2 = 'http://192.168.68.116:8000'
+: baseURL2 = 'http://192.168.68.116:8000'
 }
 
 export default baseURL2;
+
+// import { Platform } from 'react-native'
+
+
+// let baseURL2 = '';
+
+// {Platform.OS == 'android'
+// ? baseURL2 = 'http://reachtupt.online'
+// : baseURL2 = 'http://reachtupt.online'
+// }
+
+// export default baseURL2;
